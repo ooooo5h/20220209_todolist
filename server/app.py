d05f9ae98180b60d03f04f7eea9c9c773e260e32
전은형 <ooooo5h91@gmail.com>
2022-02-10 22:55:44 +0900

    create_app을 실행할 때 어떤 환경설정으로 돌릴건지 받아서 적용시킴

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
     
     CORS(app)
+    
+    app.config.from_object(f'server.config.{config_name}')
+    
     return app
