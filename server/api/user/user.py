3c6cd26f4e60488f4eb389c0b81d2ae8da86cb13
전은형 <ooooo5h91@gmail.com>
2022-02-10 23:14:56 +0900

    사용자 관련된 기능을 수행하는 클래스를 생성
    메쏘드이름을 get, post, put등 으로 만들면 알아서 자동으로 연동이 됨

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,1 +1,22 @@
 # 사용자 정보와 관련된 기능들을 모아두는 모듈
+from flask_restful import Resource
+
+class User(Resource):
+    
+    def get(self):
+        return {
+            'code' : 200,
+            'message' : '임시-사용자정보조회'
+        }
+        
+    def post(self):
+        return {
+            'code' : 200,
+            'message' : '임시-로그인기능'
+        }
+
+    def put(self):
+        return {
+            'code' : 200,
+            'message' : '임시-회원가입기능'
+        }
