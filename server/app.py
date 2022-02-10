6d751e7a8aade64b6a70d42a33234fde1775bf39
전은형 <ooooo5h91@gmail.com>
2022-02-10 23:17:42 +0900

    만든 클래스를 API로 활용할 수 있도록 도와주는 세팅을 진행

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_restful_swagger_2 import Api
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -7,5 +8,7 @@ def create_app(config_name):
     CORS(app)
     
     app.config.from_object(f'server.config.{config_name}')
+    
+    api = Api(app, api_spec_url='/api/spec', title='to_do_list', api_version='0.1', catch_all_404s=True)
     
     return app
