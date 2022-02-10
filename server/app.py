69394fb4407b6743de2feee8559b60d3c47768e2
전은형 <ooooo5h91@gmail.com>
2022-02-10 23:32:04 +0900

    swagger 문서 페이지를 만듬

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful_swagger_2 import Api
+from flask_swagger_ui import get_swaggerui_blueprint
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,5 +20,10 @@ def create_app(config_name):
     api.add_resource(Todo, '/todo')
     
     
+    # swagger문서를 자동으로 생성하기
+    swagger_ui = get_swaggerui_blueprint('/api/docs', '/api/spec.json', config={'app_name' : 'TO_DO_LIST'})
+    
+    # Flask앱의 url에 swagger문서를 등록하기
+    app.register_blueprint(swagger_ui, url_prefix='/api/docs')
     
     return app
