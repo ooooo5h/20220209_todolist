c154fc4b91254b5e6590b7ed34816ca231e23d7b
전은형 <ooooo5h91@gmail.com>
2022-02-11 00:48:42 +0900

    멍충멍충몬..
    DB상의 데드라인은 signout_at으로 만들어두고선, 파이썬에서는 reitred_at으로 써서 생긴 오류였다!!에잇!!

--- server/model/users.py
+++ server/model/users.py
@@ -10,7 +10,7 @@ class Users(db.Model):
     name = db.Column(db.String(20), nullable=False)
     nickname = db.Column(db.String(20), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    # retired_at = db.Column(db.DateTime)
+    signout_at = db.Column(db.DateTime)
     
     def get_data_object(self):
         data = {
@@ -19,7 +19,7 @@ class Users(db.Model):
             'name' : self.name,
             'nickname' : self.nickname,
             'created_at' : str(self.created_at),
-            # 'retired_at' : str(self.retired_at),
+            'signout_at' : str(self.signout_at) if self.signout_at else None,
         }
         
         return data
