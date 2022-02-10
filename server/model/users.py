836f112cbd654e3cb9cfd20c93308b9e433eb2af
전은형 <ooooo5h91@gmail.com>
2022-02-11 00:30:11 +0900

    users 테이블을 표현하는 클래스를 추가했고, 컬럼들을 가져옴

--- server/model/users.py
+++ server/model/users.py
@@ -2,3 +2,12 @@ from server import db
 
 class Users(db.Model):
     
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    u_id = db.Column(db.String(20), nullable=False)
+    u_pw = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    nickname = db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    retired_at = db.Column(db.DateTime)
