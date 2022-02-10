4d440c9c5e48c95bbeb5965ba43b15dcb9ebb518
전은형 <ooooo5h91@gmail.com>
2022-02-10 23:40:32 +0900

    todo에 관련한 기능은 태그를 다르게 써줌

--- server/api/todo/todo.py
+++ server/api/todo/todo.py
@@ -1,7 +1,23 @@
 from flask_restful import Resource
+from flask_restful_swagger_2 import swagger
 
 class Todo(Resource):
     
+    @swagger.doc({
+        'tags' : ['todo'],
+        'description' : 'to do list 생성하기',
+        'parameters' : [
+            
+        ],
+        'responses' : {
+            '200' : {
+                'description' : 'to do list 생성 성공' 
+            },
+            '400' : {
+                'description' : 'to do list 생성 실패' 
+            },    
+        }
+    })
     def get(self):
         return {
             '임시' : '임시 투두 겟'
