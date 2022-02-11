87b74511012e39c8c702b3375a0c89fdcdf88dae
전은형 <ooooo5h91@gmail.com>
2022-02-11 19:53:45 +0900

    todos를 표현하는 클래스 생성

---
+++ server/model/todos.py
@@ -0,0 +1,26 @@
+from server import db
+
+class Todos(db.Model):
+    
+    __tablename__ = 'todos'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.Foreignkey('users.id'))
+    title = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.Datetime, nullable=False)
+    duedate = db.Column(db.Date, nullable=False)
+    is_completed = db.Column(db.Boolean, nullable=False)
+    
+    def get_data_object(self):
+        data = {
+            'id' : self.id,
+            'user_id' : self.user_id,
+            'title' : self.title,
+            'content' : self.content,
+            'created_at' : str(self.created_at),
+            'duedate' : self.duedate,
+            'is_completed' : self.is_completed,
+        }
+        
+        return data
