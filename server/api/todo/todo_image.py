4eb3b39b060f66c9648d867de0d29d5e09a48d78
전은형 <ooooo5h91@gmail.com>
2022-02-13 22:05:07 +0900

    라이브러리 werkzeung의 도움을 받아 파일을 파라미터로 첨부하는 중

--- server/api/todo/todo_image.py
+++ server/api/todo/todo_image.py
@@ -1,4 +1,9 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from werkzeug.datastructures import FileStorage
+
+put_parser = reqparse.RequestParser()
+put_parser.add_argument('image', type=FileStorage, required=True, location='files', action='append')
+put_parser.add_argument('todo_id', type=int, required=True, location='form')
 
 class TodoImage(Resource):
     
