74a08bc54aeecd2abb6aae691d7f138823e97ff1
전은형 <ooooo5h91@gmail.com>
2022-02-13 22:09:08 +0900

    swagger로 파일을 첨부하는 문서만 작성했음

--- server/api/todo/todo_image.py
+++ server/api/todo/todo_image.py
@@ -1,4 +1,6 @@
 from flask_restful import Resource, reqparse
+from flask_restful_swagger_2 import swagger
+
 from werkzeug.datastructures import FileStorage
 
 put_parser = reqparse.RequestParser()
@@ -7,7 +9,36 @@ put_parser.add_argument('todo_id', type=int, required=True, location='form')
 
 class TodoImage(Resource):
     
+    @swagger.doc({
+        'tags' : ['todo'],
+        'description' : 'to do 이미지 등록하기',
+        'parameters' : [
+            {
+                'name' : 'todo_id',
+                'description' : '사진을 첨부할 todo의 id',
+                'in' : 'formData',
+                'type' : 'integer',
+                'required' : True,
+            },
+            {
+                'name' : 'image',
+                'description' : '실제로 첨부할 이미지 파일',
+                'in' : 'formData',
+                'type' : 'file',
+                'required' : True,
+            }
+        ],
+        'responses' : {
+            '200' : {
+                'description' : '사진 첨부 성공'
+            },
+            '400' : {
+                'description' : '사진 첨부 실패'
+            },
+        }
+    })
     def put(self):
+        """to do 이미지 등록하는 기능"""
         return {
             'code' : 'to do 이미지 등록하는 기능' 
         }
